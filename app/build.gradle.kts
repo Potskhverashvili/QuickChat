2c518ee89d789b9b7758dc8cdb3d7e617132887f
GFO <gia.focxverashvili@gmail.com>
2025-02-20 18:52:04 +0400

    bottom nav

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,8 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.firebase.firestore.ktx)
+    implementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.espresso.core)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
