c3d139a1138be76883e09725c8806f799c20f92a
giorgiDzagania <georgedzagania@gmail.com>
2025-02-20 14:29:36 +0400

    login

--- app/src/main/java/com/example/quickchat/core/OperationStatus.kt
+++ app/src/main/java/com/example/quickchat/core/OperationStatus.kt
@@ -3,8 +3,7 @@ package com.example.quickchat.core
 sealed interface OperationStatus<T> {
     data class Success<T>(val value: T) : OperationStatus<T>
     data class Failure<T>(val exception: Exception) : OperationStatus<T>
-    data class Loading<T>(val progress: Int? = null, val message: String? = null) :
-        OperationStatus<T>
+    data class Loading<T>(val progress: Int? = null, val message: String? = null) : OperationStatus<T>
 }
 
 fun <FromType, ToType> OperationStatus<FromType>.map(
